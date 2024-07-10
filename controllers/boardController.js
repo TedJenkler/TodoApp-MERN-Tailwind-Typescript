8b9c15b0f4514ed9223321fbd8214c708560ccd1
Ted Jenkler <tedjenkler@gmail.com>
2024-07-10 15:13:49 +0200

    Addded CRUD board

--- controllers/boardController.js
+++ controllers/boardController.js
@@ -1,5 +1,35 @@
 const Board = require('../schemas/boardSchema');
 
+exports.getAll = async (req, res) => {
+    try {
+        const boards = await Board.find();
+        if (boards.length === 0) {
+            return res.status(404).json({ message: 'No boards found' });
+        }
+        
+        res.status(200).json({ message: 'Successfully fetched boards', boards });
+    } catch (error) {
+        console.error('Error fetching all boards', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.getById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        
+        const board = await Board.findById(id);
+        if(!board) {
+            return res.status(404).json({ message: 'No board found' });
+        }
+
+        res.status(200).json({ message: 'Successfully fetched board', board});
+    }catch (error) {
+        console.error('Error fetching board', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
 exports.addBoard = async (req, res) => {
     try {
         const { name } = req.body;
@@ -19,5 +49,58 @@ exports.addBoard = async (req, res) => {
     }catch (error) {
         console.error('Error adding board', error);
         res.status(500).json({ message: 'Internal Server Error'});
+    }
+};
+
+
+exports.updateById = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const { id } = req.params;
+        if (!name) {
+            return res.status(400).json({ message: 'Name is required' });
+        }
+
+        const updatedBoard = await Board.findByIdAndUpdate(id, { name }, { new: true });
+        if (!updatedBoard) {
+            return res.status(404).json({ message: 'No board found' });
+        }
+
+        res.status(200).json({ message: 'Successfully updated board', board: updatedBoard });
+    } catch (error) {
+        console.error('Error updating board', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.deleteById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deletedBoard = await Board.findByIdAndDelete(id);
+        if (!deletedBoard) {
+            return res.status(404).json({ message: 'No board found' });
+        }
+
+        res.status(200).json({ message: 'Successfully deleted board', board: deletedBoard });
+    } catch (error) {
+        console.error('Error deleting board', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+exports.deleteAll = async (req, res) => {
+    try {
+        const boards = await Board.find();
+        if (boards.length === 0) {
+            return res.status(404).json({ message: 'No boards found' });
+        }
+
+        await Board.deleteMany();
+
+        res.status(200).json({ message: 'Successfully deleted all boards' });
+    } catch (error) {
+        console.error('Error deleting boards', error);
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 };
--- routes/boardroutes.js
+++ routes/boardroutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const boardController = require('../controllers/boardController');
 
+router.get('/', boardController.getAll);
+router.get('/:id', boardController.getById);
 router.post('/add', boardController.addBoard);
+router.put('/:id', boardController.updateById);
+router.delete('/:id', boardController.deleteById);
+router.delete('/', boardController.deleteAll);
 
 module.exports = router;
